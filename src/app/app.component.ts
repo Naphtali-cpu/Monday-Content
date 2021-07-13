e690081721f1679a37801e8bf475a90986385214
Naphtali <naphtali919@gmail.com>
2021-07-13 07:19:21 +0300

    Added some codes in app component typescript file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Goals';
+  title = 'Goals'
+}
+
+export class AppComponent {
+  title = 'My Goals'
 }
