68a0dbd10301119bd45bfe962ee267da13b2455d
Naphtali <naphtali919@gmail.com>
2021-07-13 11:45:40 +0300

    New push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,13 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+import { Component } from '@angular/core';
+import { Goal } from './goal';
+....
 export class AppComponent {
-  title = 'Goals'
-}
-
-export class AppComponent {
-  title = 'My Goals'
-}
-
-var arr = [3,5,7,11,13];
-for(var i in arr){
-  console.log(i);
+  goals:Goal[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2, name:'Buy Cookies'},
+    {id:3, name:'Get new Phone Case'},
+    {id:4, name:'Get Dog Food'},
+    {id:5, name:'Solve math homework'},
+    {id:6, name:'Plot my world domination plan'},
+  ];
 }
